f4837b060ed0c9d4a61be5c2437ca0b6cd2077cd
meg768 <magnus@egelberg.se>
2016-11-04 16:07:30 +0100

    -

--- app.js
+++ app.js
@@ -1,47 +1,91 @@
-var express = require('express')
-var app = express()
-const port = 3000
+#!/usr/bin/env node
+
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var sprintf = require('yow').sprintf;
+var bodyParser = require('body-parser');
+var redirectLogs = require('yow').redirectLogs;
+var prefixLogs = require('yow').prefixLogs;
+var MySQL = require('mysql');
+
+var Server = function(args) {
+
+	args = parseArgs();
 
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : '104.155.92.17',
-  user     : 'root',
-  password : 'potatismos',
-  database : 'strecket'
-});
-
-connection.connect();
-
-
-
-app.get('/', function (request, result) {
-	result.send('Hello World!')
-})
-
-app.get('/stocks', function (request, result) {
-
-	connection.query('SELECT * FROM aktier', function(error, rows, fields) {
-	  if (error) {
-	  	result.send({error:error});
-
-	  }
-	  else {
-	  	result.send(JSON.stringify(rows));
-	  }
-
-
+	var mysql = MySQL.createConnection({
+	  host     : '104.155.92.17',
+	  user     : 'root',
+	  password : 'potatismos',
+	  database : 'strecket'
 	});
-})
 
-//app.listen(3000);
+	mysql.connect();
+
+	function parseArgs() {
+		var commander = require('commander');
+
+		commander.version('1.0.0');
+		commander.option('-l --log', 'redirect logs to file');
+		commander.option('-p --port <port>', 'listens to specified port', 3000);
+		commander.parse(process.argv);
+
+		var args = ['port', 'log'];
+
+		args.forEach(function(item) {
+			args[item] = commander[item];
+		});
+
+		return args;
+	}
+
+	function listen() {
+		app.set('port', (args.port || 3000));
+		app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }))
+		app.use(bodyParser.json({limit: '50mb'}));
+		app.use(cors());
+
+		app.get('/stocks', function (request, response) {
+
+			mysql.query('SELECT * FROM aktier', function(error, rows, fields) {
+			  if (error) {
+				  response.status(200).json([]);
+
+			  }
+			  else {
+				  response.status(200).json(rows);
+			  }
 
 
+			});
+		})
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+		app.get('/', function (request, response) {
+			response.send('Hello World!')
+		})
 
-  console.log(`server is listening on ${port}`)
-})
+
+		app.listen(app.get('port'), function() {
+			console.log("Node app is running on port " + app.get('port'));
+		});
+
+
+	}
+
+	function run() {
+
+		prefixLogs();
+
+		if (args.log) {
+			redirectLogs();
+		}
+
+		listen();
+
+	}
+
+	run();
+}
+
+module.exports = new Server();
