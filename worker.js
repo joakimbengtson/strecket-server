5e04764b92d668a787ea680c72123276397aec8f
Joakim Bengtson <joakim@bookio.com>
2018-09-02 11:37:16 +0200

    -

--- worker.js
+++ worker.js
@@ -94,20 +94,6 @@ var Worker = module.exports = function(pool, poolMunch) {
 				  // ???
 			  });
 
-/* OLD API?!
-			client.sendSms({
-				    to: '+46703489493',
-				    from:'+46769447443',
-				    body: txtMsg
-			}, function(error, message) {
-			    if (error) {
-				    console.log("sendSMS:", error);
-					reject(error);
-			    }
-				else
-					resolve();
-			});
-*/
 		});
 	}
 
